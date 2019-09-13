d81020da2b3b6c5365b1f4f4aade953695da5eb7
Ubuntu <ubuntu@ip-172-31-14-27.ec2.internal>
2019-09-13 18:54:54 +0000

    s

--- bare-metal/model_run.py
+++ bare-metal/model_run.py
@@ -19,24 +19,23 @@ if len(sys.argv) != 5:
     print "Usage: %s <model_hostname> <model_port> <application_axf> <output_file>" % sys.argv[0]
     sys.exit()
 
-
+'''
 # Start model process running, verify it has started before moving on
 cmd = ['/arm-tools/Cortex-M33-FVP/FVP_MPS2_Cortex-M33','-C','fvp_mps2.DISABLE_GATING=1','-C','fvp_mps2.platform_type=1','--cadi-server']
 print cmd
 
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True,shell=True)
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
 # Verify model has started before moving on, or timeout
 timeout = time.time()+10 # 10 second timeout
 while True:
-	output_line = process.stdout.readline()
-	print output_line
 	if "Info: FVP_MPS2_Cortex_M33: CADI Debug Server started for ARM Models..." in output_line:
 		break
 	if time.time() > timeout:
 		print 'Error; model never seemed to start. Exiting python script.'
 		sys.exit()
 	time.sleep(0.5)
+'''
 
 # Connect to the running model on local host
 m = NetworkModel(sys.argv[1], int(sys.argv[2]))
